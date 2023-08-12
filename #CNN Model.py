ec8844836f25aabd32e2b6579d31a543f9549345
NithinMaloth <88584675+NithinMaloth@users.noreply.github.com>
2023-08-12 17:12:11 +0530

    #CNN Model.py

--- #CNN Model.py
+++ #CNN Model.py
@@ -9,13 +9,13 @@ train_data_dir = 'path_to_training_data'
 val_data_dir = 'path_to_validation_data'
 test_data_dir = 'path_to_test_data'
 
-# Define hyperparameters
+s
 batch_size = 32
 epochs = 20
 input_shape = (64, 64, 1)
 num_classes = 2  # Number of classes (normal and myocardial infarction)
 
-# Data augmentation for training data
+
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1.0 / 255,
     rotation_range=15,
@@ -31,7 +31,7 @@ train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
 val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
 test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
 
-# Load and prepare the data using generators
+
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=input_shape[:2],
@@ -56,7 +56,7 @@ test_generator = test_datagen.flow_from_directory(
     class_mode='categorical'
 )
 
-# Create the CNN model
+# CNN Model
 model = models.Sequential([
     Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape),
     Conv2D(64, (3, 3), activation='relu', padding='same'),
@@ -89,10 +89,9 @@ model = models.Sequential([
     Dense(num_classes, activation='softmax')
 ])
 
-# Compile the model
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
 history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
@@ -101,7 +100,7 @@ history = model.fit(
     validation_steps=val_generator.samples // batch_size
 )
 
-# Evaluate the model on the test set
+
 test_loss, test_acc = model.evaluate(test_generator, steps=test_generator.samples // batch_size)
 print(f"Test accuracy: {test_acc}")
 
